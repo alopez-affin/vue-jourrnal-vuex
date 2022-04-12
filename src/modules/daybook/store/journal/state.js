7c951b4558e72a58a363ac943bea8f01f3552f6e
Ángel López Gil <alopez@affin.es>
2022-04-12 16:38:23 +0200

    Segundo commit

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -10,7 +10,13 @@ export default () => ({
         {
             id: new Date().getTime() + 1000,
             date: new Date().toDateString(),
-            text: 'Incididunt culpa labore consequat Lorem sit officia tempor cillum. Do veniam nostrud anim elit non. Qui minim incididunt velit mollit esse laboris commodo sit aute. Nisi exercitation irure cillum enim voluptate. Veniam nisi et ad magna nostrud velit nostrud.',
+            text: 'Non consequat dolore nisi laboris anim in do incididunt anim. Dolor occaecat anim eiusmod commodo qui in nisi excepteur dolore magna excepteur aliqua id. Ullamco ipsum sint ad adipisicing id eiusmod qui eiusmod fugiat aliqua. Laborum aute voluptate aliquip aliquip. Magna incididunt proident irure elit incididunt officia.',
+            pictures: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Consectetur eiusmod esse quis incididunt non sit occaecat eiusmod. Elit sit reprehenderit ad ut aliqua ipsum. Enim sunt elit aliquip sit ut Lorem cillum eu enim magna. Cillum deserunt amet magna ea. Laborum ut irure occaecat aliquip magna veniam deserunt elit. Id laborum laboris id anim tempor aliquip reprehenderit consequat ipsum excepteur qui id tempor. Minim ipsum ipsum cupidatat aute laborum officia incididunt esse ea anim.',
             pictures: null,
         }
     ]
